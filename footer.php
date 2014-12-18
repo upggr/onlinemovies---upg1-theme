ab6ae98f032d21ad9d146c713e0adaba395b983a
upggr <info@upg.gr>
2014-12-18 01:17:21 -0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,15 +14,6 @@
 	<!-- end Footer -->
 </div>
 <!-- end Shell -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-55315278-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 </body>
 </html>
